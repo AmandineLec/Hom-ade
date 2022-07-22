f3daa16372a8379716bf3876471286e9918d010a
Amandine <amandine.lec@hotmail.fr>
2022-07-22 16:38:34 +0200

    Création des classes Objets et Recettes

---
+++ app/src/main/java/fil/rouge/Objet.java
@@ -0,0 +1,40 @@
+package fil.rouge;
+
+public abstract class Objet {
+
+    //#region Variables
+
+    protected int id;
+    protected String nom;
+    
+    //#endregion
+
+    //#region Constructeur
+
+    public Objet(String nom){
+        this.nom = nom;
+    }
+    public Objet(int id){
+        this.id = id;
+    }
+    //#endregion
+
+    //#region GETTER & SETTER
+    public int getId() {
+        return id;
+    }
+    public String getNom() {
+        return nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    //#endregion 
+
+    
+
+}
