03dd24de36b8269181b623176004448755258389
Marie Cartier <mariecartier50@gmail.com>
2022-07-25 11:39:02 +0200

    modif maison et maisonTest

--- app/src/main/java/fil/rouge/Maison.java
+++ app/src/main/java/fil/rouge/Maison.java
@@ -2,18 +2,15 @@ package fil.rouge;
 
 public class Maison {
     protected int id_maison;
-    protected int taille; //en mètres carrés
     protected int niveau; // début à 1
     protected int nb_pieces; // début à 1
     protected String id_ressource; // pour récupérer les ressources nécessaires à l'agrandissement
     protected int nb_ressources; // pour définir le nombre de ressources nécéssaires pour agrandir
-    protected int id_decoration; // pour récupérer la déco placée dans la maison
-    protected int id_meuble; // pour récupérer le meuble placé dans la maison
     protected String etabli; // pour accéder à l'établi lors de la création d'objets
+    protected Pieces piece; // pour accéder à la pièce
 
     //#region Constructeurs
     public Maison(){
-        this.taille = 9;
         this.niveau = 1;
     }
 
@@ -21,19 +18,6 @@ public class Maison {
         this. nb_pieces = nb_pieces;
     }
 
-    public Maison(int taille, int niveau){
-        this.taille = taille;
-    }
-    //#endregion
-
-    //#region GETSET
-    public int getTaille() {
-        return taille;
-    }
-
-    public void setTaille(int taille) {
-        this.taille = taille;
-    }
 
     public int getNiveau() {
         return niveau;
@@ -67,39 +51,37 @@ public class Maison {
         this.nb_ressources = nb_ressources;
     }
 
-    public int getId_decoration() {
-        return id_decoration;
-    }
-
-    public void setId_decoration(int id_decoration) {
-        this.id_decoration = id_decoration;
-    }
-
-    public int getId_meuble() {
-        return id_meuble;
-    }
-
-    public void setId_meuble(int id_meuble) {
-        this.id_meuble = id_meuble;
-    }
 
     public String getEtabli() {
         return etabli;
     }
 
     public void setEtabli(String etabli) {
-        this.etabli = etabli;
+        this.etabli = 
+        etabli;
+    }
+
+    public Pieces getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Pieces piece) {
+        this.piece = piece;
     }
     //#endregion
 
     //#region Méthodes
     public void levelUp(){
+        // max 15, ajout 1 niveau + agrandir
 
     }
 
-    public void agrandir(int nb_ressources){
+    public void ajoutpieces(Pieces piece){
+        // gerer ajout dans bdd
 
     }
+
+
     
     //#endregion
 }
---
+++ app/src/main/java/fil/rouge/Pieces.java
@@ -0,0 +1,71 @@
+package fil.rouge;
+
+public class Pieces {
+    protected int id_piece;
+    protected String nom;
+    protected int taille; //en mètres carrés
+    protected int id_decoration; // pour récupérer la déco placée dans la maison
+    protected int id_meuble; // pour récupérer le meuble placé dans la maison
+
+    //#region Constructeurs
+    public Pieces(){
+        this.nom = "salon";
+        this.taille = 9;
+    }
+    public Pieces(String nom){
+        this.nom = nom;
+    }
+    public Pieces(String nom, int taille){
+        this.taille = taille;
+        this.nom = nom;
+    }
+    //#endregion
+
+    //#region GETSET
+    public int getTaille() {
+        return taille;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    public int getId_decoration() {
+        return id_decoration;
+    }
+
+    public void setId_decoration(int id_decoration) {
+        this.id_decoration = id_decoration;
+    }
+
+    public int getId_meuble() {
+        return id_meuble;
+    }
+
+    public void setId_meuble(int id_meuble) {
+        this.id_meuble = id_meuble;
+    }
+
+    public int getId_piece() {
+        return id_piece;
+    }
+
+    public void setId_piece(int id_piece) {
+        this.id_piece = id_piece;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    //#endregion
+
+    //#region Méthodes
+    public void agrandir(int id_piece){
+        // récupérer table piece, créer classe piece
+    }
+    //#endregion
+}
