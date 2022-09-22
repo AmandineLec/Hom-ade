cecb934a373a789385ec9ea5e573b11a318a8b87
YannickCho <yannick.choquenet@gmail.com>
2022-09-22 15:13:51 +0200

    ajout WrongToolException

--- src/main/java/fil/rouge/service/RecoltageService.java
+++ src/main/java/fil/rouge/service/RecoltageService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fil.rouge.dao.ObjetRecoltableRepository;
+import fil.rouge.exception.WrongToolException;
 import fil.rouge.model.ObjetRecoltable;
 import fil.rouge.model.Outil;
 import fil.rouge.model.Personnage;
@@ -18,13 +19,14 @@ public class RecoltageService {
 
 
     // Simule l'utilisation d'un outil sur un objet récoltable
-    public int utiliserOutil(Personnage personnage, int objetRecoltableId, int resistance) {
+    public int utiliserOutil(Personnage personnage, int objetRecoltableId, int resistance) throws WrongToolException {
         ObjetRecoltable objetRecoltable = objetRecoltableRepository.findById(objetRecoltableId).get();
         Outil outil = personnage.getOutil();
         Set<Outil> outils = objetRecoltable.getOutils();
         
         if (!(outils.contains(outil)))
-            return -100;            // Retourne -100 si l'outil utilisé ne peut pas être utilisé sur l'objet récoltable
+            throw new WrongToolException("Vous n'utilisez pas le bon outil"); // Lance une exception si l'outil utilisé ne peut pas être utilisé sur l'objet récoltable
+                       
 
         resistance -= outil.getCapacite();
         return resistance;          // Retourne la résistance de l'objet récoltable après utilisation de l'outil
--- src/main/java/fil/rouge/service/RecolteService.java
+++ src/main/java/fil/rouge/service/RecolteService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fil.rouge.exception.WrongToolException;
 import fil.rouge.model.Personnage;
 import fil.rouge.model.RessourcesRecoltees;
 
@@ -23,7 +24,11 @@ public class RecolteService {
     
     public int recoltageRamassage(Personnage personnage, int objetRecoltableId, int resistance) {
         
-        resistance = recoltageService.utiliserOutil(personnage, objetRecoltableId, resistance);
+        try {
+            resistance = recoltageService.utiliserOutil(personnage, objetRecoltableId, resistance);
+        } catch (WrongToolException e) {
+            e.printStackTrace();
+        }
         if (resistance <= 0) {
             List<RessourcesRecoltees> listeRessources = ramassageService.listeRessourcesRamassees(objetRecoltableId);
             for (RessourcesRecoltees ressources : listeRessources) {
