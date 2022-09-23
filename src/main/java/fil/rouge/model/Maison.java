41de0a8bca92752ccef0b25e4deda11cd5ca2183
Marie Cartier <mariecartier50@gmail.com>
2022-09-23 16:51:17 +0200

    Code cleané

--- src/main/java/fil/rouge/model/Maison.java
+++ src/main/java/fil/rouge/model/Maison.java
@@ -22,30 +22,16 @@ public class Maison {
     @Column(name = "niveau")
     protected int niveau; // début à 1
 
-    @Column(name = "nb_pieces")
-    protected int nb_pieces; // début à 1
+    // @Column(name = "nb_pieces")
+    // protected int nb_pieces; // début à 1
 
     @OneToMany(mappedBy = "maison")
     protected Set<Personnage> personnages = new HashSet<Personnage>();
-
-    // @Transient
-    // protected ArrayList<Piece> pieces = new ArrayList<Piece>();
-
-    // protected String id_ressource; // pour récupérer les ressources nécessaires à l'agrandissement
-    // protected int nb_ressources; // pour définir le nombre de ressources nécéssaires pour agrandir
-    // protected String etabli; // pour accéder à l'établi lors de la création d'objets
-    // protected Piece piece; // pour accéder à la pièce
 
     //#region Constructeurs
     public Maison(){
         this.niveau = 1;
     }
-
-    // public Maison(int nb_pieces){
-    //     this. nb_pieces = nb_pieces;
-    //     this.niveau = 1;
-    //     this.piece = new Piece("salon");
-    // }
 
     //#endregion
 
@@ -58,39 +44,6 @@ public class Maison {
         this.niveau = niveau;
     }
 
-    public int getNb_pieces() {
-        return nb_pieces;
-    }
-
-    public void setNb_pieces(int nb_pieces) {
-        this.nb_pieces = nb_pieces;
-    }
-
-    // public String getId_ressource() {
-    //     return id_ressource;
-    // }
-
-    // public void setId_ressource(String id_ressource) {
-    //     this.id_ressource = id_ressource;
-    // }
-
-    // public int getNb_ressources() {
-    //     return nb_ressources;
-    // }
-
-    // public void setNb_ressources(int nb_ressources) {
-    //     this.nb_ressources = nb_ressources;
-    // }
-
-    // public String getEtabli() {
-    //     return etabli;
-    // }
-
-    // public void setEtabli(String etabli) {
-    //     this.etabli =
-    //     etabli;
-    // }
-
     public int getId_maison() {
         return id_maison;
     }
@@ -99,58 +52,4 @@ public class Maison {
         this.id_maison = id_maison;
     }
 
-    // public Piece getPiece() {
-    //     return piece;
-    // }
-
-    // public void setPiece(Piece piece) {
-    //     this.piece = piece;
-    // }
-
-    //#endregion
-
-    //#region Méthodes
-    // public void levelUp(Piece piece){
-    //     this.setNiveau(this.getNiveau()+1);
-    //     if (this.getNiveau()>=1 && this.getNiveau()<=3){
-    //         this.pieceAccessible(piece);
-    //     }
-    //     else if(this.getNiveau()>3 && this.getNiveau()%2==0){ // si le niveau est pair on ajoute une pièce
-    //         this.pieceAccessible(piece);
-    //     }
-    //     else{
-    //         piece.agrandir(1);
-    //     }
-    // }
-
-    // public void pieceAccessible(Piece piece){
-    //     this.setNb_pieces(this.getNb_pieces()+1);
-    //     this.piece = piece;
-    // }
-
-//       public static int sauvegarderMaison(){
-//         try{
-//           Maison maisonJoueur = new Maison(1);
-//           int niveauMaison = maisonJoueur.getNiveau();
-//           int nbPiecesMaison = maisonJoueur.getNb_pieces();
-//           String query = "INSERT INTO maison (niveau,nb_pieces) VALUES (?,?)";
-//           PreparedStatement myStmt = DBManager.preparedStatement(query);
-//           myStmt.setInt(1, niveauMaison);
-//           myStmt.setInt(2, nbPiecesMaison);
-//           myStmt.executeUpdate();
-//           ResultSet res = myStmt.getGeneratedKeys();
-//           res.next();
-//           int clePrimaireMaison = res.getInt(1);
-//           return clePrimaireMaison;
-
-//           } catch (SQLException ex) {
-//           // handle any errors
-//           System.out.println("SQLException: " + ex.getMessage());
-//           System.out.println("SQLState: " + ex.getSQLState());
-//           System.out.println("VendorError: " + ex.getErrorCode());
-//         }
-//       return -1;
-//   }
-
-    //#endregion
 }
