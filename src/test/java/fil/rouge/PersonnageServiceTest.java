3fe6e2e14e8652567130d69aca8098a8098fdae9
Marie Cartier <mariecartier50@gmail.com>
2022-09-27 14:08:52 +0200

    Test inscription terminé

--- src/test/java/fil/rouge/PersonnageServiceTest.java
+++ src/test/java/fil/rouge/PersonnageServiceTest.java
@@ -31,7 +31,7 @@ public class PersonnageServiceTest {
     PersonnageRepository pRepository;
 
     @Test
-    public void inscriptionFailedTest() {
+    public void WhenInvalidInscription_ThenThrowsException() {
         //Personnage personnage = new Personnage("Jean", 1, "marie@mail.fr", "1234");
         //Mockito.when(pRepository.save(personnage)).thenReturn(personnage);
 
@@ -46,30 +46,104 @@ public class PersonnageServiceTest {
         assertThrows(MailAlreadyUsedException.class, () -> pService.inscription("marie@mail.fr", "1254", "Paul", 2));
     }
 
-    // @Test
-    // public void whenValidInscription_ThenSavedPersonnageHasMaison()
-    // {
-    //     // pService.inscription()
+    @Test
+    public void whenValidInscription_ThenSavedPersonnageHasMaison()
+    {
+        try{
+            pService.inscription("marie@mail.fr", "1254", "Paul", 2);
+        }
+        catch(Exception e){
 
-    //     // Syntaxes pour faire une vérification pour un argument :
-    //     ArgumentMatcher<Personnage> matcher = this::checkPersonnageHasMaison;
-    //     matcher = x -> x.getMaison() != null;
-        
-    //     /**
-    //      * Demande à Mockito de vérifier si la méthode "save" a été appelée
-    //      * sur pRepository.
-    //      * 
-    //      * Demande à Mockito de vérifier si l'argument passé à "save" (donc un personnage)
-    //      * satisfait un test (ici "personnage.getMaison() != null")
-    //      */
-    //     Mockito
-    //         .verify(pRepository)
-    //         .save(ArgumentMatchers.argThat(this::checkPersonnageHasMaison));
-    // }
+        }
+        /* Syntaxes pour faire une vérification d'un argument de méthode :
+        * ArgumentMatcher<Personnage> matcher = this::checkPersonnageHasMaison;
+        * matcher = x -> x.getMaison() != null;
+        */
 
-    // private boolean checkPersonnageHasMaison(Personnage personnage)
-    // {
-    //     return personnage.getMaison() != null;
-    // }
+        /*
+         * Ce que fait Mockito.verify :
+         * 
+         * Demande à Mockito de vérifier si la méthode "save" a été appelée
+         * sur pRepository.
+         * 
+         * Puis demande à Mockito de vérifier si l'argument passé à "save" (donc un personnage)
+         * satisfait un test (ici "personnage.getMaison() != null")
+         */
+        Mockito.verify(pRepository).save(ArgumentMatchers.argThat(this::checkPersonnageHasMaison));
+    }
+
+    @Test
+    public void whenValidInscription_ThenSavedPersonnageHasMail()
+    {
+        try{
+            pService.inscription("marie@mail.fr", "1254", "Paul", 2);
+        }
+        catch(Exception e){
+
+        }
+        Mockito.verify(pRepository).save(ArgumentMatchers.argThat(this::checkPersonnageHasMail));
+    }
+
+    @Test
+    public void whenValidInscription_ThenSavedPersonnageHasPassword()
+    {
+        try{
+            pService.inscription("marie@mail.fr", "1254", "Paul", 2);
+        }
+        catch(Exception e){
+
+        }
+        Mockito.verify(pRepository).save(ArgumentMatchers.argThat(this::checkPersonnageHasPassword));
+    }
+
+    @Test
+    public void whenValidInscription_ThenSavedPersonnageIsFemale()
+    {
+        try{
+            pService.inscription("marie@mail.fr", "1254", "Paul", 2);
+        }
+        catch(Exception e){
+
+        }
+        Mockito.verify(pRepository).save(ArgumentMatchers.argThat(this::checkPersonnageIsFemale));
+    }
+
+    @Test
+    public void whenValidInscription_ThenSavedPersonnageIsMale()
+    {
+        try{
+            pService.inscription("marie@mail.fr", "1254", "Paul", 1);
+        }
+        catch(Exception e){
+
+        }
+        Mockito.verify(pRepository).save(ArgumentMatchers.argThat(this::checkPersonnageIsMale));
+    }
+
+
+    private boolean checkPersonnageHasMaison(Personnage personnage)
+    {
+        return personnage.getMaison() != null;
+    }
+
+    private boolean checkPersonnageHasMail(Personnage personnage)
+    {
+        return personnage.getMail() != null;
+    }
+
+    private boolean checkPersonnageHasPassword(Personnage personnage)
+    {
+        return personnage.getPassword() != null;
+    }
+
+    private boolean checkPersonnageIsFemale(Personnage personnage)
+    {
+        return personnage.getSexe() == 2;
+    }
+
+    private boolean checkPersonnageIsMale(Personnage personnage)
+    {
+        return personnage.getSexe() == 1;
+    }
 }
 
