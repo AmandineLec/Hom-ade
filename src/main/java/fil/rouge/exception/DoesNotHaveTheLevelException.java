df18bc88a656a6684e3ad474cd7eadb03522fea1
Amandine <amandine.lec@hotmail.fr>
2022-09-27 14:10:28 +0200

    Validation des exceptions liés aux recettes

---
+++ src/main/java/fil/rouge/exception/DoesNotHaveTheLevelException.java
@@ -0,0 +1,8 @@
+package fil.rouge.exception;
+
+public class DoesNotHaveTheLevelException extends Exception {
+
+    public DoesNotHaveTheLevelException(String message){
+        super(message);
+    }
+}
