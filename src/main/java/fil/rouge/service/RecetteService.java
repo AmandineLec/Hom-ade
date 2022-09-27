5466d6c83c3eb08738f61e0c01d371a8f7146d98
Amandine <amandine.lec@hotmail.fr>
2022-09-27 14:09:42 +0200

    Création des fichiers d'excception lié aux recettes

--- src/main/java/fil/rouge/service/RecetteService.java
+++ src/main/java/fil/rouge/service/RecetteService.java
@@ -39,26 +39,36 @@ public class RecetteService {
     public boolean fusionnerRessource(int idObj, Personnage personnage){
         //On instancie un objet en getReferenceById car on ne sait pas à l'avance quel objet on veut créer. 
         Objet objet = oRepository.getReferenceById(idObj);
+
         //On va chercher en BDD toutes les lignes composant une recette en fonction de l'id de l'objet que l'on veut créer
         List<Recette> recettes = recetteRepository.findByObjet(objet);
+
         //POur chaque ligne présente dans la recette
         for (Recette recette : recettes){
+
             //On vérifie si le niveau de la maison du joueur est le même que le niveau de le recette si oui ou continue, si non, on ne peut pas créer l'objet.
             if( personnage.getMaison().getNiveau() == recette.getNiveau_requis())
+
             //Pour chaque ressource présente dans l'inventaire du personnage
                 for(InventaireRessource inventaireRessource : personnage.getInventaireRessource()){
+
                     //On vérifie que la ressource est présente dans l'inventaire du personnage est en bonne quantité. Si oui, on continue si non, on ne peut pas créer l'objet. 
                     if(recette.getRessource().getId() == inventaireRessource.getRessource().getId() && recette.getQuantite_necessaire() <= inventaireRessource.getQuantite()){ 
+
                         //On instancie une variable quantité, calculant la quantité de ressource utilisée pour crée l'objet. 
                         int quantite = inventaireRessource.getQuantite() - recette.getQuantite_necessaire();
+
                         //On appelle la méthode "retirerRessource", permettant d'instancier la nouvelle quantité de ressource du personnage de la ressource utlisée
+
                         inventaireRessourceService.retirerRessource(inventaireRessource.getRessource().getId(),quantite, personnage);
+
                         //Finalement, on crée l'objet, et on l'ajoute à l'inventaire du personnage. 
                         objetService.creerObjet(personnage, idObj);                   
                         return true; 
                     }                
                 }
             }
+            if(personnage.getMaison().getNiveau() == recette.getNiveau_requis())
         return false;
     }
 }
