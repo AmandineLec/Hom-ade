313fd929aa5f61383e575a23f0905a70b8d1a9f5
Marie Cartier <mariecartier50@gmail.com>
2022-09-27 10:27:58 +0200

    Tests PersonnageRepository OK

---
+++ src/test/java/fil/rouge/givenPersonnageWithMail_WhenFindingByMail_ThenReturnPersonnage.sql
@@ -0,0 +1,17 @@
+INSERT INTO personnage (nom, mail, sexe, password)
+values
+(
+	'Joe',
+    'joe@aa.fr',
+    '1',
+    '1323'
+);
+
+INSERT INTO personnage (nom, mail, sexe, password)
+values
+(
+	'Paul',
+    'paul@aa.fr',
+    '1',
+    '1323'
+);
