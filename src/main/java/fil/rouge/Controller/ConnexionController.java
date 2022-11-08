7378f6618f91aa51588625fcbbeddcd593d43da3
Selom <selom.ya@outlook.fr>
2022-11-08 17:00:17 +0100

    removed cross origin from connexion controller

--- src/main/java/fil/rouge/Controller/ConnexionController.java
+++ src/main/java/fil/rouge/Controller/ConnexionController.java
@@ -3,7 +3,6 @@ package fil.rouge.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +12,6 @@ import fil.rouge.model.Personnage;
 import fil.rouge.service.PersonnageService;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:4200")
 public class ConnexionController {
 	@Autowired
 	public PersonnageService pService;
