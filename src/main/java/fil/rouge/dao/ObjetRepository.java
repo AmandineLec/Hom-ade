c70f67525494be2d0870812d9a68ba8a4263a36c
Amandine <amandine.lec@hotmail.fr>
2022-11-10 14:27:52 +0100

    Json controller pour les recettes

--- src/main/java/fil/rouge/dao/ObjetRepository.java
+++ src/main/java/fil/rouge/dao/ObjetRepository.java
@@ -1,6 +1,5 @@
 package fil.rouge.dao;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +10,4 @@ import fil.rouge.model.Objet;
 
 @Repository
 public interface ObjetRepository extends JpaRepository<Objet, Integer>{
-
 }
