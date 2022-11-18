7332f97afa42cafdc7e894d38bc569691d2645cd
Marie Cartier <mariecartier50@gmail.com>
2022-11-18 11:28:03 +0100

    coms effacés

--- src/main/java/fil/rouge/Controller/ConnexionController.java
+++ src/main/java/fil/rouge/Controller/ConnexionController.java
@@ -25,59 +25,19 @@ public class ConnexionController {
 	@Autowired
 	public PersonnageRepository pRepo;
 
-	
-
-
-
-
-	// @PostMapping("/first_connexion") // via cet url, récupere les infos du joueur -> bouton "c'est parti" page connexion.html
-	// public String firstConnexion(Model model){
-	// 	return "/login"; // Affiche la page partie.html
-	// }
-
 	@PostMapping("/new_connexion") // Via l'url /connexion -> bouton connexion page inscription.html
 	public String continuePartie(Model model, @ModelAttribute PersonnageDto personnage) throws Exception {
 		model.addAttribute("personnage", personnage);
 		return "/login"; // Accede à la page connexion.html
 	}
 
-	// @GetMapping("/login") // Affiche la page de connexion via cet url
-	// public String connexion(Model model) throws Exception {
-	// 	return "/login"; // page connexion.html
-	// }
-
 	@PostMapping(value= "/connection", consumes = "application/json",  produces = "application/json") // via cet url, récupere les infos du joueur -> bouton "c'est parti" page connexion.html
 	@ResponseBody
 	public PersonnageDto getPartie(@RequestBody  PersonnageDto personnage, Model model) throws Exception {
 		
 		model.addAttribute("personnage", personnage);
-		// System.out.println(personnage.getMail());
-		// System.out.println(personnage.getPassword());
 		pService.connexionPartie(personnage.getMail(), personnage.getPassword());
 		return personnage; // Affiche la page partie.html
-		// https://www.tutorialspoint.com/how-to-write-create-a-json-file-using-java#:~:text=write(jsonObject.,into%20a%20file%20named%20output.
-	}	// https://www.baeldung.com/spring-mvc-set-json-content-type
+	}
 
-	@RequestMapping("/user")
-	public Principal user(Principal user) {
-		return user;
-	}
-	
-/*
-@RequestMapping(
-	path = "/connection", 
-	method = RequestMethod.POST,
-	consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, 
-	produces = {
-	MediaType.APPLICATION_ATOM_XML_VALUE, 
-	MediaType.APPLICATION_JSON_VALUE
-	})
-	public  @ResponseBody PersonnageDto  getPartie(@ModelAttribute PersonnageDto personnage, Model model){
-		model.addAttribute("personnage", personnage);
-		System.out.println(personnage.getMail());
-		System.out.println(personnage.getPassword());
-		pService.connexionPartie(personnage.getMail(), personnage.getPassword());
-		return personnage; // Affiche la page partie.html
-	}
-	/* */
 }
